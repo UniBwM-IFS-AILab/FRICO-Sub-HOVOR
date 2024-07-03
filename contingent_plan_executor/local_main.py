4553c31fc1d2bcb2495cf8fd18d517a94e4db46e
pj <prakash.jamakatel@unibw.de>
2024-07-03 09:03:11 +0200

    updated paths

--- contingent_plan_executor/local_main.py
+++ contingent_plan_executor/local_main.py
@@ -8,10 +8,10 @@ def run_local_conversation(output_files_path):
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) > 1:
-    #     arg = sys.argv[1]
-    # else:
-    #     raise ValueError("Please provide the directory to your plan4dial output files as a system argument.")
-    #/home/qnc/Plan4Dial/contingent-plan-executor/local_data/gold_standard_data
-    arg = "/home/qnc/Plan4Dial/plan4dial/plan4dial/local_data/conversation_alignment_bots/ijcai_bot/output_files"
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+    else:
+        raise ValueError("Please provide the directory to your plan4dial output files as a system argument.")
+    /home/qnc/Plan4Dial/contingent-plan-executor/local_data/gold_standard_data
+    #arg = "/home/qnc/Plan4Dial/plan4dial/plan4dial/local_data/conversation_alignment_bots/ijcai_bot/output_files"
     run_local_conversation(arg)
